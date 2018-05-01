8c3bfe0d7fe5390b49a5bf7d1e3fbae79c0bc5a7
willvg <willvg1127@gmail.com>
2018-05-01 14:47:34 -0600

    Agrego la parte de lectura de imagenes de la base de datos y ademas de formar la primer estructura de datos

---
+++ ManagerClusterNode/Resources.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Resources.cpp
+ * Author: will
+ * 
+ * Created on 1 de mayo de 2018, 02:45 PM
+ */
+
+#include "Resources.h"
+
+Resources::Resources() {
+}
+
+Resources::Resources(const Resources& orig) {
+}
+
+Resources::~Resources() {
+}
+
---
+++ ManagerClusterNode/build/Debug/GNU-Linux/Image/ImageGetter.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/Image/ImageGetter.o: Image/ImageGetter.cpp \
+ Image/ImageGetter.h
+
+Image/ImageGetter.h:

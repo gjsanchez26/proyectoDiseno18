4117504b5cda479b216452d38ed11b6e9c47d23a
gjsanchez26 <gjsanchez26@gmail.com>
2018-06-01 15:37:21 -0600

    No images

---
+++ DistributedTrees/build/Debug/GNU-Linux/_ext/7e3bde50/QueueThread.o.d
@@ -0,0 +1,23 @@
+build/Debug/GNU-Linux/_ext/7e3bde50/QueueThread.o: \
+ /home/gabo/Documents/DistributedTrees/QueueThread.cpp \
+ /home/gabo/Documents/DistributedTrees/QueueThread.h \
+ /home/gabo/Documents/DistributedTrees/Estructura.h \
+ /home/gabo/Documents/DistributedTrees/Task.h \
+ /home/gabo/Documents/DistributedTrees/FeaturesMat.h \
+ /home/gabo/Documents/DistributedTrees/Features.h \
+ /home/gabo/Documents/DistributedTrees/Config.h \
+ /home/gabo/Documents/DistributedTrees/Scheduler.h
+
+/home/gabo/Documents/DistributedTrees/QueueThread.h:
+
+/home/gabo/Documents/DistributedTrees/Estructura.h:
+
+/home/gabo/Documents/DistributedTrees/Task.h:
+
+/home/gabo/Documents/DistributedTrees/FeaturesMat.h:
+
+/home/gabo/Documents/DistributedTrees/Features.h:
+
+/home/gabo/Documents/DistributedTrees/Config.h:
+
+/home/gabo/Documents/DistributedTrees/Scheduler.h:
---
+++ DistributedTrees/build/Debug/GNU-Linux/_ext/7e3bde50/Scheduler.o.d
@@ -0,0 +1,26 @@
+build/Debug/GNU-Linux/_ext/7e3bde50/Scheduler.o: \
+ /home/gabo/Documents/DistributedTrees/Scheduler.cpp \
+ /home/gabo/Documents/DistributedTrees/Scheduler.h \
+ /home/gabo/Documents/DistributedTrees/Estructura.h \
+ /home/gabo/Documents/DistributedTrees/QueueThread.h \
+ /home/gabo/Documents/DistributedTrees/Task.h \
+ /home/gabo/Documents/DistributedTrees/FeaturesMat.h \
+ /home/gabo/Documents/DistributedTrees/Features.h \
+ /home/gabo/Documents/DistributedTrees/Config.h \
+ /home/gabo/Documents/DistributedTrees/QueueTask.h
+
+/home/gabo/Documents/DistributedTrees/Scheduler.h:
+
+/home/gabo/Documents/DistributedTrees/Estructura.h:
+
+/home/gabo/Documents/DistributedTrees/QueueThread.h:
+
+/home/gabo/Documents/DistributedTrees/Task.h:
+
+/home/gabo/Documents/DistributedTrees/FeaturesMat.h:
+
+/home/gabo/Documents/DistributedTrees/Features.h:
+
+/home/gabo/Documents/DistributedTrees/Config.h:
+
+/home/gabo/Documents/DistributedTrees/QueueTask.h:

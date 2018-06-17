d20796ec0f2c627210b9474ff62acd63d09118af
willvg <willvg1127@gmail.com>
2018-06-17 10:02:39 -0600

    Update Resources.h

--- DistributedTrees/Resources.h
+++ DistributedTrees/Resources.h
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Resources.h
- * Author: will
+ * Authors: 
+ * Gabriel Sanchez
+ * Alejandro Gonzalez
+ * Wilberth Varela
  *
- * Created on 1 de mayo de 2018, 02:45 PM
  */
 
 #ifndef RESOURCES_H
@@ -24,23 +20,29 @@
 
 #include "Resource.h"
 
-class Resources {
-public:
-    Resources();
-    Resources(const Resources& orig);
-    virtual ~Resources();
-    void getResources(std::vector<std::string>& infoNodoCluster);
-    void createObjet(std::vector<std::string> infoNodoCluster, rdf::Resource &resourcesNodo);
-private:
-    void createDocuments();
-    void loadDocuments(std::ifstream& meminfo, std::ifstream& cpuinfo);
-    void readDocument(std::ifstream& file, std::string& text, int tipo); // tipo 0=memoria 1= cpu
-    void writeDocument(std::string final);
-    void deleteDocument();  //remove( "myfile.txt" )
-    void readDocumentEnd(std::vector<std::string>& vector);
-    void convetir();
+namespace rdf {
 
-};
+    /*
+     * This class load in the object resource, the resources of each cluster node
+     */
+    class Resources {
+    public:
+        Resources();
+        Resources(const Resources& orig);
+        virtual ~Resources();
+        void getResources(std::vector<std::string>& infoNodoCluster);
+        void createObjet(std::vector<std::string> infoNodoCluster, rdf::Resource &resourcesNodo);
+    private:
+        void createDocuments();
+        void loadDocuments(std::ifstream& meminfo, std::ifstream& cpuinfo);
+        void readDocument(std::ifstream& file, std::string& text, int tipo); // tipo 0=memoria 1= cpu
+        void writeDocument(std::string final);
+        void deleteDocument(); //remove( "myfile.txt" )
+        void readDocumentEnd(std::vector<std::string>& vector);
+        void convert();
+
+    };
+}
 
 #endif /* RESOURCES_H */
 

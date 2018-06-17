5350556568e2cfda5268a9184e59a24014ff581f
willvg <willvg1127@gmail.com>
2018-06-17 11:02:12 -0600

    Update Estructura.cpp

--- DistributedTrees/Estructura.cpp
+++ DistributedTrees/Estructura.cpp
@@ -1,32 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Estructura.cpp
- * Author: will
- * 
- * Created on 8 de mayo de 2018, 02:59 PM
- */
 
 #include "Estructura.h"
 
-
-Estructura::Estructura() {
+/**
+ * Contructor
+ */
+rdf::Estructura::Estructura() {
 }
 
-Estructura::Estructura(const Estructura& orig) {
+rdf::Estructura::Estructura(const Estructura& orig) {
 }
 
-Estructura::~Estructura() {
+rdf::Estructura::~Estructura() {
 }
 
-void Estructura::seeImageDepth(int height, int width, cv::Mat imageDepth){
+/**
+ * Funtion see to depth image
+ * @param height
+ * @param width
+ * @param imageDepth
+ */
+void rdf::Estructura::seeImageDepth(int height, int width, cv::Mat imageDepth){
     cv::Mat depTem;
     depTem = cv::Mat(height, width, CV_16UC1, cv::Scalar(0));
-    
     int mayor = 0;
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -48,11 +43,15 @@ void Estructura::seeImageDepth(int height, int width, cv::Mat imageDepth){
     }
     cv::namedWindow( "Original image", CV_WINDOW_AUTOSIZE ); 
     cv::imshow( "Original image", depTem ); 
-    cv::waitKey(2000);
+    cv::waitKey(10000);
 }
 
-void Estructura::seeImageLabel(cv::Mat& imageLabel){
+/**
+ * Funtion see to label image
+ * @param imageLabel
+ */
+void rdf::Estructura::seeImageLabel(cv::Mat& imageLabel){
     cv::namedWindow( "Original image", CV_WINDOW_AUTOSIZE ); 
     cv::imshow( "Original image", imageLabel ); 
-    cv::waitKey(2000);
+    cv::waitKey(10000);
 }

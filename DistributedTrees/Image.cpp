b86ba5d9462db4f4a33474f51d28d2dc32788a30
willvg <willvg1127@gmail.com>
2018-06-17 11:05:17 -0600

    Update Image.cpp

--- DistributedTrees/Image.cpp
+++ DistributedTrees/Image.cpp
@@ -1,46 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Image.cpp
- * Author: will
- * 
- * Created on 8 de mayo de 2018, 08:53 PM
- */
-
 #include "Image.h"
 #include "ImageGetter.h"
 #include "Collection.h"
 #include "PointsSelect.h"
 
-Image::Image() {
+using namespace rdf;
+
+rdf::Image::Image() {
 }
 
-Image::Image(const Image& orig) {
+rdf::Image::Image(const Image& orig) {
 }
 
-Image::~Image() {
+rdf::Image::~Image() {
 }
 
-void Image::getStructura(std::vector<Estructura::Node>& structure, Estructura::DataMaster dataMaster) {
+/**
+ * Funtion get to structure of diferents algorithms
+ * @param structure
+ * @param dataMaster
+ */
+void rdf::Image::getStructura(std::vector<Estructura::Node>& structure, Estructura::DataMaster dataMaster) {
     std::string PathDepthImage = "/home/RDF/code/DistributedTrees/Database/pathDepth.txt";
-    std::string PathLabelImage = "/home/RDF/code/DistributedTrees/Database/pathLabel.txt";
+    std::string PathLabelImage = "/home/RDF/code/DistributedTrees/Database/pathDepth.txt";
 
 
     std::vector<Estructura::Node> structureTemp;
+    
+    //load structure of the shotton algorithm
     if (dataMaster.type_Algorithm.compare(0, 7, "shotton") == 0) {
         generatedStructureShotton(structureTemp, dataMaster.type_Algorithm, dataMaster.NumPoints, dataMaster.height, dataMaster.width,
                 dataMaster.NumTree, PathDepthImage, PathLabelImage, dataMaster.startRead, dataMaster.endRead, dataMaster.tree, dataMaster.node);
     }
-
-
     structure = structureTemp;
 }
 
-void Image::generatedStructureShotton(std::vector<Estructura::Node>& structure,
+/**
+ * Funtion make the structure of shotton algorithm
+ * @param structure
+ * @param type_Algorithm
+ * @param NumPoints
+ * @param height
+ * @param width
+ * @param NumTree
+ * @param pathDepth
+ * @param pathlabel
+ * @param startRead
+ * @param endRead
+ * @param tree
+ * @param node
+ */
+void rdf::Image::generatedStructureShotton(std::vector<Estructura::Node>& structure,
         std::string type_Algorithm, int NumPoints, int height, int width,
         int NumTree, std::string pathDepth, std::string pathlabel,
         int startRead, int endRead, int tree, int node) {
@@ -58,12 +67,9 @@ void Image::generatedStructureShotton(std::vector<Estructura::Node>& structure,
 
     imageGetter.readPath(directionesDepth, NumLines, pathDepth, startRead, endRead);
     imageGetter.readPath(directionesLabel, NumLines, pathlabel, startRead, endRead);
-
-
-
-    // de aqui para arriba esta bien
-
-    for (int k = 0; k < directionesDepth.size(); k++) {
+    
+    //carga las imagenes cona las direcciones
+    for (unsigned int k = 0; k < directionesDepth.size(); k++) {
 
         Estructura::Node dataAll;
         std::vector<Estructura::Pixel> puntos;
@@ -86,20 +92,21 @@ void Image::generatedStructureShotton(std::vector<Estructura::Node>& structure,
         //es.seeImageDepth(height,width,imageDepth);
         //es.seeImageLabel(imageLabel);
 
-
-
+        
 
         pointSelect.generatedPoints(type_Algorithm, puntos, NumPoints, height, width, NumTree);
 
         //llena los campos y carga al vector
+
         dataAll.imageDepth = imageDepth;
         dataAll.imageLabel = imageLabel;
         dataAll.points = puntos;
         dataAll.node = node;
         dataAll.tree = tree;
+
         structure.push_back(dataAll);
-        
     }
 
 
 }
+

c7d2ffc83f0dc8255fd0353246de4dc05f3afda6
willvg <willvg1127@gmail.com>
2018-06-17 10:59:05 -0600

    Update Collection.cpp

--- DistributedTrees/Collection.cpp
+++ DistributedTrees/Collection.cpp
@@ -1,35 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Collection.cpp
- * Author: will
- * 
- * Created on 1 de mayo de 2018, 09:06 PM
- */
-
 #include "Collection.h"
 
-Collection::Collection() {
+rdf::Collection::Collection() {
 }
 
-Collection::Collection(const Collection& orig) {
+rdf::Collection::Collection(const Collection& orig) {
 }
 
-Collection::~Collection() {
+rdf::Collection::~Collection() {
 }
 
 
-void Collection::setImage(StrategyImage* TypeImage){
-    Collection::stImage = TypeImage;
+/**
+ * This method works to change image, by load in the structure
+ * @param TypeImage
+ */
+void rdf::Collection::setImage(StrategyImage* TypeImage){
+    rdf::Collection::stImage = TypeImage;
 }
 
-void Collection::readImage(int height, int width, std::string direction, cv::Mat& image){
+/**
+ * This method works to read image of the database
+ * @param height
+ * @param width
+ * @param direction
+ * @param image
+ */
+void rdf::Collection::readImage(int height, int width, std::string direction, cv::Mat& image){
     cv::Mat imageTem;
-    Collection::stImage->readImage(height,width,direction,imageTem);
+    rdf::Collection::stImage->readImage(height,width,direction,imageTem);
     image = imageTem;
     
 }

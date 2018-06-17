3a22798ed461fdc056332a54bc2678e5452107df
willvg <willvg1127@gmail.com>
2018-06-17 09:54:47 -0600

    Update Collection.h

--- DistributedTrees/Collection.h
+++ DistributedTrees/Collection.h
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Collection.h
- * Author: will
+ * Authors: 
+ * Gabriel Sanchez
+ * Alejandro Gonzalez
+ * Wilberth Varela
  *
- * Created on 1 de mayo de 2018, 09:06 PM
  */
 
 #ifndef COLLECTION_H
@@ -17,18 +13,23 @@
 #include <string>
 #include "StrategyImage.h"
 
+namespace rdf {
 
-class Collection {
-public:
-    Collection();
-    Collection(const Collection& orig);
-    virtual ~Collection();
-    void setImage(StrategyImage* TypeImage);
-    void readImage(int height, int width, std::string direction, cv::Mat& image);
-private:
-    StrategyImage* stImage;
+    /**
+     *This class works for implement the abstraction of diferent images  
+     * 
+     */
+    class Collection {
+    public:
+        Collection();
+        Collection(const Collection& orig);
+        virtual ~Collection();
+        void setImage(StrategyImage* TypeImage);
+        void readImage(int height, int width, std::string direction, cv::Mat& image);
+    private:
+        StrategyImage* stImage;
 
-};
+    };
 
+}
 #endif /* COLLECTION_H */
-

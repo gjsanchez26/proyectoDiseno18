8eab9500d5d1c39c76728ac28f0c0ad756866d59
willvg <willvg1127@gmail.com>
2018-06-17 11:01:29 -0600

    Update DepthImage.cpp

--- DistributedTrees/DepthImage.cpp
+++ DistributedTrees/DepthImage.cpp
@@ -1,32 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   DepthImage.cpp
- * Author: will
- * 
- * Created on 1 de mayo de 2018, 09:20 PM
- */
-
 #include "DepthImage.h"
 
-DepthImage::DepthImage() {
+
+rdf::DepthImage::DepthImage() {
 }
 
-DepthImage::DepthImage(const DepthImage& orig) {
+rdf::DepthImage::DepthImage(const DepthImage& orig) {
 }
 
-DepthImage::~DepthImage() {
+rdf::DepthImage::~DepthImage() {
 }
 /**
- * Funcion para la lectura de la imagen de profundidad.
+ * Funtion to read depth image
  * @param direction
  * @param imageLabel
  */
-void DepthImage::readImage(int height, int width, std::string direction, cv::Mat& image) {
+void rdf::DepthImage::readImage(int height, int width, std::string direction, cv::Mat& image) {
     cv::Mat imegeDepth;
     imegeDepth = cv::Mat(height, width, CV_16UC1, cv::Scalar(0));
     std::string rute = direction;

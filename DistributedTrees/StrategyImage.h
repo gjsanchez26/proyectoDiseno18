60a0efe2364e54c773df9ee8eaf109966b69f910
willvg <willvg1127@gmail.com>
2018-06-17 10:05:42 -0600

    Update StrategyImage.h

--- DistributedTrees/StrategyImage.h
+++ DistributedTrees/StrategyImage.h
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   StrategyImage.h
- * Author: will
+ * Authors: 
+ * Gabriel Sanchez
+ * Alejandro Gonzalez
+ * Wilberth Varela
  *
- * Created on 1 de mayo de 2018, 08:54 PM
  */
 
 #ifndef STRATEGYIMAGE_H
@@ -20,16 +16,21 @@
 #include <fstream>      // std::ifstream
 #include <iostream>     // std::cout
 
-class StrategyImage {
-public:
-    StrategyImage();
-    StrategyImage(const StrategyImage& orig);
-    virtual ~StrategyImage();
-    virtual void readImage(int heigth, int width, std::string direction, cv::Mat& image) = 0;
-    
-private:
+namespace rdf {
+    /*
+     * This class implements methods abstract to the load image
+     */
+    class StrategyImage {
+    public:
+        StrategyImage();
+        StrategyImage(const StrategyImage& orig);
+        virtual ~StrategyImage();
+        virtual void readImage(int heigth, int width, std::string direction, cv::Mat& image) = 0;
 
-};
+    private:
+
+    };
+}
 
 #endif /* STRATEGYIMAGE_H */
 

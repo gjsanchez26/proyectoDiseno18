c3fe0284585c68b9f80b00a11bf51aacf4c61e1e
willvg <willvg1127@gmail.com>
2018-06-17 11:07:18 -0600

    Update LabelImage.cpp

--- DistributedTrees/LabelImage.cpp
+++ DistributedTrees/LabelImage.cpp
@@ -1,25 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   LabelImage.cpp
- * Author: will
- * 
- * Created on 1 de mayo de 2018, 09:00 PM
- */
-
 #include "LabelImage.h"
+using namespace rdf;
 
-LabelImage::LabelImage() {
+rdf::LabelImage::LabelImage() {
 }
 
-LabelImage::LabelImage(const LabelImage& orig) {
+rdf::LabelImage::LabelImage(const LabelImage& orig) {
 }
 
-LabelImage::~LabelImage() {
+rdf::LabelImage::~LabelImage() {
 }
 
 /**
@@ -27,7 +15,7 @@ LabelImage::~LabelImage() {
  * @param direction
  * @param imageLabel
  */
-void LabelImage::readImage(int height, int width, std::string direction, cv::Mat& image) {
+void rdf::LabelImage::readImage(int height, int width, std::string direction, cv::Mat& image) {
     cv::Mat imageLbl(height, width, CV_8UC3);
     std::string rute = direction;
     imageLbl = cv::imread(rute.c_str());

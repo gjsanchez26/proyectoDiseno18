df8904a434f27c362e97a79d201c37f263d07f90
willvg <willvg1127@gmail.com>
2018-06-17 09:59:40 -0600

    Update LabelImage.h

--- DistributedTrees/LabelImage.h
+++ DistributedTrees/LabelImage.h
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
+//* 
  * File:   LabelImage.h
- * Author: will
+ * Authors: 
+ * Gabriel Sanchez
+ * Alejandro Gonzalez
+ * Wilberth Varela
  *
- * Created on 1 de mayo de 2018, 09:00 PM
  */
 
 #ifndef LABELIMAGE_H
@@ -20,18 +16,25 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+namespace rdf {
 
-class LabelImage: public StrategyImage {
-public:
-    LabelImage();
-    LabelImage(const LabelImage& orig);
-    virtual ~LabelImage();
-    virtual void readImage(int height, int width, std::string direction, cv::Mat& image);
+    /*
+     * This class load label image 
+     */
+    class LabelImage : public StrategyImage {
+    public:
+        LabelImage();
+        LabelImage(const LabelImage& orig);
+        virtual ~LabelImage();
+        virtual void readImage(int height, int width, std::string direction, cv::Mat& image);
 
-    
-private:
 
-};
+    private:
+
+    };
+}
 
 #endif /* LABELIMAGE_H */
+
+
 

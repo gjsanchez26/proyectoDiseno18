d417de18adcfbbc432a2d1edb0865864da4bf95e
willvg <willvg1127@gmail.com>
2018-06-17 09:56:18 -0600

    Update Estructura.h

--- DistributedTrees/Estructura.h
+++ DistributedTrees/Estructura.h
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   Estructura.h
- * Author: will
+ * Authors: 
+ * Gabriel Sanchez
+ * Alejandro Gonzalez
+ * Wilberth Varela
  *
- * Created on 8 de mayo de 2018, 02:59 PM
  */
 
 #ifndef ESTRUCTURA_H
@@ -21,46 +18,55 @@
 #include <stdio.h>
 #include <vector>
 
-class Estructura {
-public:
-    Estructura();
-    Estructura(const Estructura& orig);
-    virtual ~Estructura();
-    struct Pixel{
-        cv::Point point;
-        std::vector<int> ubicacion;
-    } pixel;
-    struct Node {
-        cv::Mat imageDepth;
-        cv::Mat imageLabel;
-        std::vector<Estructura::Pixel> points;
-        std::vector<int> treesAvailability;
-        int tree;
-        int node;
-        void getTotalSize(int& valor){
-            valor = sizeof(imageDepth) + sizeof(imageLabel) + sizeof(points) + sizeof(tree)+ sizeof(node);
-        }
-    } node;
-    
-    struct DataMaster {
-        std::string type_Algorithm;
-        int NumPoints;
-        int height;
-        int width;
-        int NumTree; 
-        int startRead;
-        int endRead;
-        int tree;
-        int node;
-    } dataMaster;
-    
-    void seeImageDepth(int height, int width, cv::Mat imageDepth);
-    void seeImageLabel(cv::Mat& imageLabel);
-    
-private:
-    
+namespace rdf {
 
-};
+    /*
+     * This class works to create the structure of diferent images 
+     */
+    class Estructura {
+    public:
+        Estructura();
+        Estructura(const Estructura& orig);
+        virtual ~Estructura();
+
+        struct Pixel {
+            cv::Point point;
+            std::vector<int> ubicacion;
+        } pixel;
+
+        struct Node {
+            cv::Mat imageDepth;
+            cv::Mat imageLabel;
+            std::vector<Estructura::Pixel> points;
+            std::vector<int> treesAvailability;
+            int tree;
+            int node;
+
+            void getTotalSize(int& valor) {
+                valor = sizeof (imageDepth) + sizeof (imageLabel) + sizeof (points) + sizeof (tree) + sizeof (node);
+            }
+        } node;
+
+        struct DataMaster {
+            std::string type_Algorithm;
+            int NumPoints;
+            int height;
+            int width;
+            int NumTree;
+            int startRead;
+            int endRead;
+            int tree;
+            int node;
+        } dataMaster;
+
+        void seeImageDepth(int height, int width, cv::Mat imageDepth);
+        void seeImageLabel(cv::Mat& imageLabel);
+
+    private:
+
+
+    };
+}
 
 #endif /* ESTRUCTURA_H */
 
